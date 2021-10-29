f141fe27f8ff77398359f4dbcc20e43e213615e6
Hicabi YILMAZ <yilmazhicabi@gmail.com>
2021-10-29 21:37:57 +0300

    helpimal case stud

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import {BrowserRouter as Router,Route,Switch,useHistory} from "react-router-dom";
 import './App.css';
+import {useState} from 'react';
+import Login from './components/Login/Index';
+import UserList from './components/UserList/Index';
 
 function App() {
+ const [authorized,setAuthorized] = useState(false);
+  let history = useHistory();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   {/* <Login/> */}
+    //   {/* <UserList/> */}
+    //   <
+    // </div>
+
+    <Router>
+      <Switch>
+        <Route exact path="/" component={()=><Login setAuthorized={setAuthorized} authorized={authorized}/>}/>
+        <Route exact path="/userlist" component={()=><UserList authorized={authorized}/>}/>
+      </Switch>
+    </Router>
   );
 }
 
